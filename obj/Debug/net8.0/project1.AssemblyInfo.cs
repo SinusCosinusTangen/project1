166c675e5fbf42585309922bf373f733c8453c75
James Frederix Rolianto <jamesfrederix16@gmail.com>
2024-10-14 21:24:38 +0700

    delete appsettings dev

--- obj/Debug/net8.0/project1.AssemblyInfo.cs
+++ obj/Debug/net8.0/project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7421f7456a13249d53d4c2ac926a23060db077e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee3e26b4e10b915a034c357301fe93a780c5779")]
 [assembly: System.Reflection.AssemblyProductAttribute("project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
